1fb2560e5205b9b2af50eaa8553553fb694eec13
Orlando Alves <88433941+orlando762@users.noreply.github.com>
2023-06-16 15:55:01 -0300

    Update search.py

--- search.py
+++ search.py
@@ -1,12 +1,12 @@
 import requests
-
+#Lista todos os commits
 def listar_commits(usuario, repositorio):
     url = f"https://api.github.com/repos/{usuario}/{repositorio}/commits"
     headers = {"Accept": "application/vnd.github.v3+json"}
     
     response = requests.get(url, headers=headers)
     commits = response.json()
-    
+    #pega a resposta em json e valida se o status é 200
     if response.status_code == 200:
         for commit in commits:
             autor = commit["commit"]["author"]["name"]
@@ -15,7 +15,7 @@ def listar_commits(usuario, repositorio):
             print(f"Mensagem: {mensagem}")
             print("---")
     else:
-        print(f"Erro ao buscar commits: {response.status_code}")
+        print(f"Erro ao buscar commits: {response.status_code}") #em caso de erro retorna erro.
 
 
 # Exemplo de uso
